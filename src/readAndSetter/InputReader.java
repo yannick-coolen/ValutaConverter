d6781497d18481d4fc39c1553cd1a9829c1ac79b
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2023-01-06 01:15:18 +0100

    Updated the class InputReader

--- src/readAndSetter/InputReader.java
+++ src/readAndSetter/InputReader.java
@@ -2,35 +2,35 @@ package readAndSetter;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Scanner;
 
 public class InputReader {
     /**
-     *
-     * @throws IOException
+     * Biedt de mogelijkheid om een bestaand bestand te openen
+     * als de invoer van de gebruiker gelijk is met het bestaand bestand
+     * @throws IOException Als de invoer niet gelijk is met het bestaand bestand.
+     * treed er een Exception waarbij deze een bericht terug geeft dat het bestand
+     * niet bestaat, en voert vervolgens het programma niet uit.
      */
     public static void readIn() throws IOException {
         System.out.println("Voer de naam van het bronbestand in die je wilt openen:");
         Scanner scanBronBestand = new Scanner(System.in);
         String inputBronBestand = scanBronBestand.nextLine(); // product
         Path bronPath = Path.of("C:\\Users\\yanni\\IdeaProjects\\ValutaConverter\\" + inputBronBestand + ".txt");
-
         try {
             String alleTekst = Files.readString(bronPath);
 
             if (Files.exists(bronPath) && Files.isRegularFile(bronPath)) {
                 System.out.println(alleTekst);
                 System.out.println();
+
                 // class OutputSetter
                 OutputSetter.setOutput(bronPath);
-            } else {
-                throw new IOException(bronPath.getFileName().toString());
             }
             scanBronBestand.close();
-        } catch (NoSuchFileException e) {
-            System.out.println("bronbestand '" + bronPath.getFileName().toString() + "' is onbekend");
+        } catch (IOException e) {
+            System.out.println("bronbestand: '" + bronPath.getFileName().toString() + "' is onbekend");
         }
     }
 }
