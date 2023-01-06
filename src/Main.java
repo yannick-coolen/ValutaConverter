3f190e8bef0ca1a1559c917c4ca154750913b775
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2023-01-06 01:14:43 +0100

    Modified the Main class by setting up the IOException

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ import java.io.IOException;
 import readAndSetter.InputReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        InputReader.readIn();}
+    /**
+     * Initïeer de applicatie door de klasse InputReader aan te roepen,
+     * dat de methode readIn() uitvoert
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Initiatiepunt
+        try {
+            InputReader.readIn();
+        } catch (IOException e) {
+            System.out.printf(e.getMessage());
+        }
+    }
 }
